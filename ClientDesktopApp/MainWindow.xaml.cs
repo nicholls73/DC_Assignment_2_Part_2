7d20ba0707aa14a2c911f33c6b7ca57e904ae62e
James Nicholls <jamnich73@gmail.com>
2022-10-17 19:10:52 +0800

    Started assignment.

---
+++ ClientDesktopApp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using RestSharp;
+
+namespace ClientDesktopApp {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        private static int jobsDone = 0;
+        private static string pythonCode;
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true) {
+                pythonCode = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+    }
+}
---
+++ WebServer/Controllers/ServiceController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebServer.Models;
+
+namespace WebServer.Controllers {
+    [RoutePrefix("services")]
+    public class ServiceController : ApiController {
+        [Route("getnewid")]
+        [HttpGet]
+        public int GetNewId() {
+            int highestId = 1;
+
+            foreach (Client currClient in GetListOfClients()) {
+                if (currClient.Id >= highestId) {
+                    highestId = currClient.Id + 1;
+                }
+            }
+            return highestId;
+        }
+
+        private List<Client> GetListOfClients() {
+            clientdbEntities db = new clientdbEntities();
+            return db.Clients.ToList();
+        }
+    }
+}

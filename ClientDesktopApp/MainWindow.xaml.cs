0ba0fe6b3778cd43eefcdd24259aca19a511e516
James Nicholls <jamnich73@gmail.com>
2022-10-17 19:43:16 +0800

    Added ClientLib

--- ClientDesktopApp/MainWindow.xaml.cs
+++ ClientDesktopApp/MainWindow.xaml.cs
@@ -15,23 +15,45 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
 using RestSharp;
+using ClientLib;
+using Newtonsoft.Json;
 
 namespace ClientDesktopApp {
+    public delegate List<Client> GetClients();
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        private readonly static string WEBSERVER_URL = "http://localhost:58988/";
+
         private static int jobsDone = 0;
         private static string pythonCode;
 
         public MainWindow() {
             InitializeComponent();
         }
-        private void Button_Click(object sender, RoutedEventArgs e) {
+
+        private void UploadButton_Click(object sender, RoutedEventArgs e) {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true) {
                 pythonCode = File.ReadAllText(openFileDialog.FileName);
             }
+        }
+
+        private async void LookForNewClients() {
+
+            Task<List<Client>> task = new Task<List<Client>>(GetClients);
+        }
+
+        private List<Client> GetClients() {
+            List<Client> clientList = new List<Client>();
+
+            RestClient restClient = new RestClient(WEBSERVER_URL);
+            RestRequest restRequest = new RestRequest("api/Clients/", Method.Get);
+            RestResponse restResponse = restClient.Execute(restRequest);
+
+            return JsonConvert.DeserializeObject<List<Client>>(restResponse.Content);
         }
     }
 }
